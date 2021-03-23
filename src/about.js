f5ed14f5a49925e94dec1657e141be2489940152
muhammad anis ur rehman <muhammad.anis@vectracom.com>
2021-03-24 01:23:17 +0500

    apply add to calendar

--- src/about.js
+++ src/about.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-
-const About = ()=> {
-        return (
-            <div>
-                <h1>About Page</h1>        
-            </div>
-        );
+import {Button, Dropdown} from 'react-bootstrap';
+import AddToCalendar from 'react-add-to-calendar';
+let items = [
+    { google: 'Google' }
+ ];
+class About extends React.Component {
+    static displayName = 'Example';
+    state = {
+        event: {
+        title: 'Sample Event 2',
+        description: 'This is the sample event provided as an example only',
+        location: 'Portland, OR',
+        startTime: '2016-09-16T20:15:00-04:00',
+        endTime: '2016-09-16T21:45:00-04:00'
     }
+};
 
-export default About;
+render() {
+      return(
+        <AddToCalendar 
+        event={this.state.event}
+        // buttonTemplate={icon}
+        listItems={items}
+        displayItemIcons = {false} //not work
+      //   useFontAwesomeIcons={false} //not work
+      //   optionsOpen = {true} //work
+        />
+      )
+    };
+  }
+  export default About;
